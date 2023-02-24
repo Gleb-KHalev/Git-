9501116cec2c6937688fb2309e119d91cf09b6de
Gleb-Khalev <khaevga@gmail.com>
2023-02-24 21:43:15 +0600

    git commit

--- main.py
+++ main.py
@@ -36,13 +36,9 @@ class Example(QMainWindow):
             qp.end()
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    sys.excepthook = except_hook
     ex = Example()
     ex.show()
     sys.exit(app.exec_())
